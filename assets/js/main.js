db2e3557f2c44fbff703a4603718a231bc8b4631
Ardeman <ardeman@gokampus.com>
2022-01-17 07:10:23 +0700

    fix css url path

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,43 +3,43 @@ $(document).ready(() => {
         dots: true,
         slidesToShow: 7,
         infinite: false,
-        responsive:[
+        responsive: [
             {
                 breakpoint: 1536,
-                settings:{
-                    slidesToShow: 6
-                }
+                settings: {
+                    slidesToShow: 6,
+                },
             },
             {
                 breakpoint: 1280,
-                settings:{
-                    slidesToShow: 5
-                }
+                settings: {
+                    slidesToShow: 5,
+                },
             },
             {
                 breakpoint: 1024,
-                settings:{
-                    slidesToShow: 4
-                }
+                settings: {
+                    slidesToShow: 4,
+                },
             },
             {
                 breakpoint: 768,
-                settings:{
-                    slidesToShow: 3
-                }
+                settings: {
+                    slidesToShow: 3,
+                },
             },
             {
                 breakpoint: 640,
-                settings:{
-                    slidesToShow: 2
-                }
+                settings: {
+                    slidesToShow: 2,
+                },
             },
             {
                 breakpoint: 376,
-                settings:{
-                    slidesToShow: 1
-                }
-            }
-        ]
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
     });
 });
